ae594a0b5fd66b3d5557e5895035b2464d1c2e4e
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-08-09 16:53:35 +0100

    Update KothCommand.php

--- src/koth/KothCommand.php
+++ src/koth/KothCommand.php
@@ -6,83 +6,86 @@
  * Time: 
  */
 namespace koth;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
+use pocketmine\command\{CommandSender, Command};
 use pocketmine\Player;
 class KothCommand extends Command
 {
     private $plugin;
-    public function __construct($name, KothMain $main){
-        parent::__construct($name, "");
+    public function __construct(KothMain $main){
         $this->plugin = $main;
     }
-    public function execute(CommandSender $sender, string $commandLabel, array $args) : bool{
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
         if ($sender instanceof Player){
-            if (isset($args[0])){
-                if (strtolower($args[0]) === "join"){
+            if (strtolower($command->getName()) === "koth") {
+                if (empty($args)) {
+                    $sender->sendMessage("§aPlease use: §b/koth help §6for a list of koth help commands.");
+                    return true;
+                }
+            }
+             if (strtolower($args[0]) == 'help') {
+                 $sender->sendMessage("§6Koth commands help \n\n§b/koth join - §aJoin a koth game event");
+             }
+                 if (isset($args[0])){
+                if (strtolower($args[0]) === 'join'){
                     if ($this->plugin->sendToKoth($sender)){
                         $sender->sendMessage($this->plugin->getData("Joined"));
                         return false;
                     }else{
                         $sender->sendMessage($this->plugin->getData("Not_Running"));
-                    }
                     return true;
-                } else if (strtolower($args[0]) === "setspawn"){
-                    if (!$sender->hasPermission("koth.start")) return true;
+                    }
+                }
+                if (strtolower($args[0]) === 'setspawn'){
+                    if (!$sender->hasPermission("koth.start")){
                     $this->plugin->setPoint($sender,"Spawn");
                     $sender->sendMessage("§dSuccessfully Added SpawnPoint");
                     return true;
-                } else if (strtolower($args[0]) === "p1"){
-                    if (!$sender->hasPermission("koth.start")) return true;
+                }
+                if (strtolower($args[0]) === 'p1'){
+                    if (!$sender->hasPermission("koth.start")){
                     $this->plugin->setPoint($sender,"p1");
                     $sender->sendMessage("§dSuccessfully Added P1 Point §5(Make Sure To Set P2)");
-                } else if (strtolower($args[0]) === "p2"){
-                    if (!$sender->hasPermission("koth.start")) return true;
+                        return true;
+                    }
+                }
+                if (strtolower($args[0]) === 'p2'){
+                    if (!$sender->hasPermission("koth.start")){
                     $this->plugin->setPoint($sender,"p2");
                     $sender->sendMessage("§dSuccessfully Added P2 Point");
-                } else if (strtolower($args[0]) === "start"){
-                    if (!$sender->hasPermission("koth.start")) return true;
+                    return true;
+                    }
+                }
+                if (strtolower($args[0]) === 'start'){
+                    if (!$sender->hasPermission("koth.start")){
                     if ($this->plugin->startArena()){
                         $sender->sendMessage("§dKoth Event Started");
+                        return true;
                     }else{
                         $sender->sendMessage("§2No Koth Arena Fully SetUp");
+                        return true;
                     }
-                } else if (strtolower($args[0]) === "stop"){
-                    if (!$sender->hasPermission("koth.stop")) return true;
+                    }
+                }
+                if (strtolower($args[0]) === 'stop'){
+                    if (!$sender->hasPermission("koth.stop")){
                     if ($this->plugin->forceStop()){
                         $sender->sendMessage("§cKoth Event Force Stopped");
+                        return true;
                     }else{
                         $sender->sendMessage("§cNo Koth Arena Fully SetUp");
+                        return true;
                     }
                 } else{
                     if ($sender->isOp()) $this->sendHelp($sender);
                     if (!$sender->isOp()) $sender->sendMessage($this->plugin->prefix()."§aJoin Game With §b/koth Join");
+                        return true;
                 }
-            }else{
-                if ($sender->isOp()) $this->sendHelp($sender);
-                if (!$sender->isOp()) $sender->sendMessage($this->plugin->prefix()."§aJoin Game With §b/koth Join");
-            }
-        }else{
-            if (isset($args[0])){
-                if (strtolower($args[0]) === "start"){
-                    if ($this->plugin->startArena()){
-                        $sender->sendMessage("§dKoth Event Started");
-                    }else{
-                        $sender->sendMessage("§cNo Koth Arena Fully SetUp");
-                    }
-                    return true;
-                } else if (strtolower($args[0]) === "stop"){
-                    if ($this->plugin->forceStop()){
-                        $sender->sendMessage("§dKoth Event Force Stopped");
-                    }else{
-                        $sender->sendMessage("§cNo Koth Arena Fully SetUp");
-                    }
-                    return true;
+                }
                 }
             }
             $sender->sendMessage("§cError Cant Run That In Console");
+            return true;
         }
-        return true;
     }
     public function sendHelp(CommandSender $sender){
         $sender->sendMessage("§7--- §a§lKoth §b§lCommands§r§7---");
@@ -92,5 +95,6 @@ class KothCommand extends Command
         $sender->sendMessage("3) §b/koth p2 - §aset point 2 for capture area");
         $sender->sendMessage("§b/koth start - §astarts KOTH Match");
         $sender->sendMessage("§b/koth stop - §aforce stop KOTH Math");
+        return true;
     }
 }
